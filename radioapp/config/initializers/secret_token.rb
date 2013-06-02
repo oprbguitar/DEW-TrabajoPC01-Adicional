ecd52df9bffcabc1bd1f59322939f1ef7657cf00
Pierre RB <oprbguitar@gmail.com>
2013-06-01 19:16:01 -0500

    creacion del proyecto

---
+++ radioapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radioapp::Application.config.secret_token = '1eb4aac77a5fbc0570aff20a6f33787bbc51c075083b3ae7fd5eaab1eec08fba09a4266712324dc434d7e12f17bf6af65c5e62141ae48ce41889d2b836c80f37'
